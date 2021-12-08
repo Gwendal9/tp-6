1dd0e891d9539ca8fd8348dad9cd0dcf9ba885c4
Gwendal Rolland <gwendal.rolland.auditeur@lecnam;etgit config --global user.email gwendal.rolland.auditeur@lecnam.net>
2021-12-08 11:37:24 +0100

    Exercice 2

--- src/com/company/Division.java
+++ src/com/company/Division.java
@@ -1,10 +1,31 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Division {
     int a,b;
 
     public double division1(int a, int b){
         return a/b;
+    }
+
+    public double division2(int a, int b){
+        double resultat = 0;
+        try{
+            resultat = a / b;
+        } catch (Exception e) {
+            System.out.println("non divisible par 0");
+        }
+        return resultat;
+    }
+
+    public double division3(int a, int b) throws IOException{
+        try{
+            return (a / b;
+        } catch (ArithmeticException e) {
+            throw new IOException();
+        }
+
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,28 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void exercice1(int a,int b) {
         Division div1 = new Division();
         System.out.println(div1.division1(a,b));
-
-
-
-
     }
+
+    public static void exercice2(int a, int b){
+        Division div2 = new Division();
+        System.out.println(div2.division2(a,b));
+    }
+
+    public  static void exercice3(int a, int b) throws IOException{
+        Division div3 = new Division();
+        System.out.println(div3.division3(a,b));
+    }
+
+
     public static void main(String[] args){
         exercice1(10,0);
-        
-
-
+        exercice2(10,0);
+        exercice3(10,0);
     }
 }

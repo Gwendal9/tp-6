27d9f4dd020507d989b17233a5244afee22f683a
Gwendal Rolland <gwendal.rolland.auditeur@lecnam;etgit config --global user.email gwendal.rolland.auditeur@lecnam.net>
2021-12-08 10:32:07 +0100

    Exercice 1

---
+++ src/com/company/Division.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Division {
+    int a,b;
+
+    public double division1(int a, int b){
+        return a/b;
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,18 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void exercice1(int a,int b) {
+        Division div1 = new Division();
+        System.out.println(div1.division1(a,b));
+
+
+
+
+    }
+    public static void main(String[] args){
+        exercice1(10,0);
+        
+
+
     }
 }
